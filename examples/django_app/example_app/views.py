a235d7f73f2232b95b901d3b3253c7dc10eba70c
User <pcyuen98@gmail.com>
2021-01-22 13:06:43 +0800

    updated covid

--- examples/django_app/example_app/views.py
+++ examples/django_app/example_app/views.py
@@ -39,7 +39,8 @@ class ChatterBotApiView(View):
         if not text.lower().find("covid") == -1:
             return JsonResponse({
                 'text': [
-                    'Total Cases 3170 (Thu Jan 21 00:00:00 MYT 2021) '
+                    'Total Cases 3170 (Thu Jan 21 00:00:00 MYT 2021) ' 
+               
                 ]
             }, status=200)
             
